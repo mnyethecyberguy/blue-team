128c0098bd98b1f8085b99e440206df879ac15f3
Michael Nye <mnye06@gmail.com>
2022-10-22 16:26:58 -0400

    Updated Get-500 function

--- scripts/Get-500.ps1
+++ scripts/Get-500.ps1
@@ -2,4 +2,9 @@
 # This script finds the built-in Administrator account in AD
 #
 
-Get-ADUser -Identity “$(((Get-ADDomain).domainsid).ToString())-500”
+function Get-500 {
+
+    Get-ADUser -Identity “$(((Get-ADDomain).domainsid).ToString())-500”
+}
+
+Get-500
