f1fefdd3fb48220847b49e638fad46eea4245ca1
Michael Nye <mnye11@icloud.com>
2024-08-05 22:42:27 -0400

    Update basic_beacon to function

--- scripts/basic_beacon.ps1
+++ scripts/basic_beacon.ps1
@@ -2,20 +2,26 @@
 # This script emulates a simple network beacon
 #
 
-$beacon_destination = "google.com"
-$beacon_port = 80
-$beacon_interval = 30
+function Start-BasicBeacon {
+    param (
+        [string]$Destination = "google.com",
+        [int]$Port = 80,
+        [int]$Interval = 30
+    )
 
-$count = 1
+    $count = 1
 
-Do {
-    if(Test-Connection -TargetName $beacon_destination -TcpPort $beacon_port) {
-        $time = Get-Date -Format T
-        Write-Host "Connection Successful at $time - Count of $count"
-    } else {
-        $time = Get-Date -Format T
-        Write-Host "Connection Failed at $time - Count of $count"
-    }
-    $count++
-    Start-Sleep -Seconds $beacon_interval
-} While (1 -eq 1)
+    Do {
+        if(Test-Connection -TargetName $Destination -TcpPort $Port) {
+            $time = Get-Date -Format T
+            Write-Host "Connection to $Destination Successful at $time - Count of $count"
+        } else {
+            $time = Get-Date -Format T
+            Write-Host "Connection to $Destination Failed at $time - Count of $count"
+        }
+        $count++
+        Start-Sleep -Seconds $Interval
+    } While (1 -eq 1)
+
+}
+
